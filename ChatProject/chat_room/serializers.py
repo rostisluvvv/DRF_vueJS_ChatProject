31b6c7111b67de7c0f8b561cd1acaf7960943ac1
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-13 12:39:23 +0300

    add UserSerializers

--- ChatProject/chat_room/serializers.py
+++ ChatProject/chat_room/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
-from .models import User, Room, Chat
+from .models import Room
+
+
+class UserSerializers(serializers.ModelSerializer):
+    """Serialization Users."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username')
 
 
 class RoomSerializers(serializers.ModelSerializer):
-    """serializers chat room"""
+    """Serialization chat room."""
+
+    creator = UserSerializers()
+    invited = UserSerializers(many=True)
 
     class Meta:
         model = Room
