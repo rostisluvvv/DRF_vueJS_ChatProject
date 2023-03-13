9174562e75a497c0ccb0b6ef5fc2c7622c96df4f
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-13 12:13:04 +0300

    add Room views

--- ChatProject/chat_room/views.py
+++ ChatProject/chat_room/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
 
-# Create your views here.
+from .models import Room
+from .serializers import RoomSerializers
+
+
+class Room(APIView):
+
+    def get(self, request):
+        rooms = Room.objects.all()
+        serializer = RoomSerializers(rooms, many=True)
+        return Response(serializer.data)
+
+
