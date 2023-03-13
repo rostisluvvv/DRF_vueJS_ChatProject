9055b8ae2894f67de9ff661031573002cb77cc24
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-13 12:59:12 +0300

    add and register ChatAdmin

--- ChatProject/chat_room/admin.py
+++ ChatProject/chat_room/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Room
+from .models import Room, Chat
 
 
 class RoomAdmin(admin.ModelAdmin):
@@ -11,4 +11,11 @@ class RoomAdmin(admin.ModelAdmin):
         return '\n'.join([user.username for user in obj.invited.all()])
 
 
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('room', 'user', 'text', 'date')
+
+
+
+
 admin.site.register(Room, RoomAdmin)
+admin.site.register(Chat, ChatAdmin)
